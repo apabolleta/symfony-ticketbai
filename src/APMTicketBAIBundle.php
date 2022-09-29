3e1eb876bb9d2b078822ee75d005fe443ba195e5
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-09-29 16:29:36 +0200

    [NEW] Create main bundle class

---
+++ src/APMTicketBAIBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace APM\TicketBAIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle main class definition.
+ *
+ * @package  apabolleta/ticketbai-bundle
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+class APMTicketBAIBundle extends Bundle
+{
+}
