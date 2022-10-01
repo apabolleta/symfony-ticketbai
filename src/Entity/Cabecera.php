705b87c10c1715494dac15489d070295fba27b43
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-10-01 14:37:37 +0200

    [EDIT] Update Cabecera entity PHPDoc

--- src/Entity/Cabecera.php
+++ src/Entity/Cabecera.php
@@ -18,6 +18,7 @@ class Cabecera
      * Identificación de la versión de la estructura del fichero TicketBAI utilizado.
      * Formato:             Alfanumérico(5)
      * Valores posibles:    L0
+     * Obligatorio:         Sí
      *
      * @access  private
      * @var     string
