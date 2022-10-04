1a269dfda00cbbe477fb1cc35132e25496885b55
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-10-05 00:54:10 +0200

    [NEW] Create ImporteRectificacionSustitutiva entity

---
+++ src/Entity/ImporteRectificacionSustitutiva.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace APM\TicketBAIBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class to define TicketBAI system 'ImporteRectificacionSustitutiva' structure.
+ *
+ * @package  apabolleta/ticketbai-bundle
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+class ImporteRectificacionSustitutiva
+{
+    /**
+     * Base imponible de la factura sustituida.
+     *
+     * Formato:         Decimal(12,2)
+     * Obligatorio:     Sí
+     *
+     * @access  private
+     * @var     string
+     *
+     * @Assert\NotBlank
+     * @Assert\Type(type="numeric")
+     * @Assert\Length(
+     *      max = 15
+     * )
+     * @Assert\Regex("/^[0-9]{1,12}([,][0-9]{1,2})?$/")
+     */
+    private string $BaseRectificada;
+
+    /**
+     * Cuota repercutida de la factura sustituida.
+     *
+     * Formato:         Decimal(12,2)
+     * Obligatorio:     Sí
+     *
+     * @access  private
+     * @var     string
+     *
+     * @Assert\NotBlank
+     * @Assert\Type(type="numeric")
+     * @Assert\Length(
+     *      max = 15
+     * )
+     * @Assert\Regex("/^[0-9]{1,12}([,][0-9]{1,2})?$/")
+     */
+    private string $CuotaRectificada;
+
+    /**
+     * Cuota del recargo de equivalencia de la factura sustituida.
+     *
+     * Formato:         Decimal(12,2)
+     * Obligatorio:     No
+     *
+     * @access  private
+     * @var     string
+     *
+     * @Assert\Type(type="numeric")
+     * @Assert\Length(
+     *      max = 15
+     * )
+     * @Assert\Regex("/^[0-9]{1,12}([,][0-9]{1,2})?$/")
+     */
+    private string $CuotaRecargoRectificada;
+
+    public function getBaseRectificada(): string
+    {
+        return $this->BaseRectificada;
+    }
+
+    public function setBaseRectificada(string $BaseRectificada): self
+    {
+        $this->BaseRectificada = $BaseRectificada;
+
+        return $this;
+    }
+
+    public function getCuotaRectificada(): string
+    {
+        return $this->CuotaRectificada;
+    }
+
+    public function setCuotaRectificada(string $CuotaRectificada): self
+    {
+        $this->CuotaRectificada = $CuotaRectificada;
+
+        return $this;
+    }
+
+    public function getCuotaRecargoRectificada(): ?string
+    {
+        return $this->CuotaRecargoRectificada;
+    }
+
+    public function setCuotaRecargoRectificada(string $CuotaRecargoRectificada): self
+    {
+        $this->CuotaRecargoRectificada = $CuotaRecargoRectificada;
+
+        return $this;
+    }
+}
