59482cc4753b917876388e6defc91cd32dff95f0
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-10-05 01:06:15 +0200

    [NEW] Create FacturaRectificativa entity

---
+++ src/Entity/FacturaRectificativa.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace APM\TicketBAIBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+use APM\TicketBAIBundle\Entity\ImporteRectificacionSustitutiva;
+
+/**
+ * Class to define TicketBAI system 'FacturaRectificativa' structure.
+ *
+ * @package  apabolleta/ticketbai-bundle
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+class FacturaRectificativa
+{
+    /**
+     * Código que identifica el tipo de factura rectificativa.
+     *
+     * Formato:             Alfanumérico(2)
+     * Valores posibles:    L7
+     * Obligatorio:         Sí
+     *
+     * @access  private
+     * @var     string
+     *
+     * @Assert\NotBlank
+     * @Assert\Type(type="alnum")
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 2
+     * )
+     * @Assert\Choice(choices=APM\TicketBAIBundle\TicketBAI::L7_Codigo)
+     */
+    private string $Codigo;
+
+    /**
+     * Identifica si el tipo de factura rectificativa es por sustitución o por diferencias.
+     *
+     * Formato:             Alfanumérico(1)
+     * Valores posibles:    L8
+     * Obligatorio:         Sí
+     *
+     * @access  private
+     * @var     string
+     *
+     * @Assert\NotBlank
+     * @Assert\Type(type="alnum")
+     * @Assert\Length(
+     *      min = 1,
+     *      max = 1
+     * )
+     * @Assert\Choice(choices=APM\TicketBAIBundle\TicketBAI::L8_Tipo)
+     */
+    private string $Tipo;
+
+    /**
+     * Obligatorio:         No
+     *
+     * @access  private
+     * @var     ImporteRectificacionSustitutiva
+     *
+     * @Assert\Type(type="ImporteRectificacionSustitutiva")
+     * @Assert\Valid
+     */
+    private ImporteRectificacionSustitutiva $ImporteRectificacionSustitutiva;
+
+    public function getCodigo(): string
+    {
+        return $this->Codigo;
+    }
+
+    public function setCodigo(string $Codigo): self
+    {
+        $this->Codigo = $Codigo;
+
+        return $this;
+    }
+
+    public function getTipo(): string
+    {
+        return $this->Tipo;
+    }
+
+    public function setTipo(string $Tipo): self
+    {
+        $this->Tipo = $Tipo;
+
+        return $this;
+    }
+
+    public function getImporteRectificacionSustitutiva(): ?ImporteRectificacionSustitutiva
+    {
+        return $this->ImporteRectificacionSustitutiva;
+    }
+
+    public function setImporteRectificacionSustitutiva(ImporteRectificacionSustitutiva $ImporteRectificacionSustitutiva): self
+    {
+        $this->ImporteRectificacionSustitutiva = $ImporteRectificacionSustitutiva;
+
+        return $this;
+    }
+}
