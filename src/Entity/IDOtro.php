6aa27e900cc7678e350f2a45e261890b91c3b6e6
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-10-19 16:30:42 +0200

    [EDIT] Import TicketBAI class

--- src/Entity/IDOtro.php
+++ src/Entity/IDOtro.php
@@ -4,6 +4,8 @@ namespace APM\TicketBAIBundle\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
+use APM\TicketBAIBundle\TicketBAI;
+
 /**
  * Class to define TicketBAI system 'IDOtro' structure.
  *
@@ -48,7 +50,7 @@ class IDOtro
      *      min = 2,
      *      max = 2
      * )
-     * @Assert\Choice(choices=APM\TicketBAIBundle\TicketBAI::L2_IDType)
+     * @Assert\Choice(choices=TicketBAI::L2_IDType)
      */
     private string $IDType;
 
