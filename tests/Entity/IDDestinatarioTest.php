9d349716b5886f9a4709f61d06c93bddbbb1a6ee
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-10-23 19:34:06 +0200

    [NEW] Create IDDestinatarioTest test entity

---
+++ tests/Entity/IDDestinatarioTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace APM\TicketBAIBundle\Tests\Entity;
+
+use APM\TicketBAIBundle\Tests\Entity\TestEntity;
+use APM\TicketBAIBundle\Entity\IDDestinatario;
+use APM\TicketBAIBundle\Entity\IDOtro;
+
+/**
+ * Class to perform TicketBAI system 'IDDestinatario' structure tests.
+ *
+ * @package  apabolleta/ticketbai-bundle
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+final class IDDestinatarioTest extends TestEntity
+{
+    public function testValidIDDestinatario(): void
+    {
+        $idDestinatario = new IDDestinatario();
+        $idDestinatario
+            ->setNIF("12345678A")
+            ->setApellidosNombreRazonSocial("Pabolleta Martorell, Asier");
+
+        self::assertIsValid($idDestinatario);
+
+        $idOtro = new IDOtro();
+        $idOtro
+            ->setCodigoPais("BR")
+            ->setIDType("04")
+            ->setID("12345678A");
+
+        $idDestinatario = new IDDestinatario();
+        $idDestinatario
+            ->setIDOtro($idOtro)
+            ->setApellidosNombreRazonSocial("Company Name S.L.")
+            ->setCodigoPostal("12345")
+            ->setDireccion("C/ Calle, 5, 1º Izquierda");
+
+        self::assertIsValid($idDestinatario);
+    }
+
+    public function testNotValidIDDestinatario(): void
+    {
+        $idDestinatario = new IDDestinatario();
+
+        self::assertIsNotValid($idDestinatario);
+    }
+}
