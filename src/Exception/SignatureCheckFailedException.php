8864eb081d781c6f6a5c2d2503cbccc042319334
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-11-21 21:45:49 +0100

    [FIX] Fix class naming and namespacing for custom exception classes according to PSR-4

--- src/Exception/SignatureCheckFailedException.php
+++ src/Exception/SignatureCheckFailedException.php
@@ -3,17 +3,6 @@
 namespace APM\TicketBAIBundle\Exception;
 
 /**
- * Class for ValidationFailedException.
- *
- * Exception thrown when validation fails.
- *
- * @package  apabolleta/symfony-ticketbai
- * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
- *
- */
-class ValidationFailedException extends \Exception {}
-
-/**
  * Class for SignatureCheckFailedException.
  *
  * Exception thrown when signature check fails.
---
+++ src/Exception/ValidationFailedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace APM\TicketBAIBundle\Exception;
+
+/**
+ * Class for ValidationFailedException.
+ *
+ * Exception thrown when validation fails.
+ *
+ * @package  apabolleta/symfony-ticketbai
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+class ValidationFailedException extends \Exception {}
