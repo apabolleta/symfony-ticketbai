7514f3d526c9bcbdfaf1277749920876c3c2dab4
Asier Pabolleta Martorell <apabolleta@gmail.com>
2022-11-30 00:19:00 +0100

    [NEW] Create custom XMLSchemaValidationFailedException

---
+++ src/Exception/XMLSchemaValidationFailedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace APM\TicketBAIBundle\Exception;
+
+/**
+ * Class for XMLSchemaValidationFailedException.
+ *
+ * Exception thrown when XML Schema validation fails.
+ *
+ * @package  apabolleta/symfony-ticketbai
+ * @author   Asier Pabolleta Martorell <apabolleta@gmail.com>
+ *
+ */
+class XMLSchemaValidationFailedException extends \Exception {}
